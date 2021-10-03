c45f920f7f54a7d99a90b25cb83cde25b6c6c688
yasersharifi <yassersharifi74@gmail.com>
2021-10-03 12:25:32 +0330

    connect to database with PDO

---
+++ DbConnection.php
@@ -0,0 +1,31 @@
+<?php
+
+class DbConnection
+{
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+    protected $connection;
+
+    public function __construct()
+    {
+        $this->host = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->database = "shoppingCart";
+
+        if (! $this->connection) {
+            try {
+                $this->connection = new PDO("mysql:host={$this->host};dbname={$this->database}", $this->username, $this->password);
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $this->connection;
+            }
+            catch (PDOException $error) {
+                return $error->getMessage();
+            }
+        }
+        return false;
+    }
+}
